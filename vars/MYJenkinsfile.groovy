1d5ebb95caebf7fb87a62c80d29a493b4c94818b
haridas.kakunje <haridas.kakunje@tarento.com>
2019-05-15 21:39:47 +0530

    Jenkinsfile updated

--- vars/MYJenkinsfile.groovy
+++ vars/MYJenkinsfile.groovy
@@ -1,7 +1,27 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
-}
 
+def call() {
+  node {
+    stage('Checkout') {
+      checkout scm
+    }
+    def p = pipelineCfg()
 
+    if (p.runTests == true) {
+      docker.image(p.testImage).inside() {
+        stage('Test') {
+          sh 'pip install -r requirements.txt'
+          sh p.testCommand
+        }
+      }
+    }
+
+    if (env.BRANCH_NAME == 'master' && p.deployUponTestSuccess == true) {
+      docker.image(p.deployToolImage).inside {
+        stage('Deploy') {
+          sh "echo ${p.deployCommand} ${p.deployEnvironment}"
+        }
+      }
+    }
+  }
