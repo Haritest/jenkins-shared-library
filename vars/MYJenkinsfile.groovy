2ae58ec86d6dd405855f202d33f20c6e5ffab02b
haridas.kakunje <haridas.kakunje@tarento.com>
2019-05-16 16:08:29 +0530

    Dockerfile added

--- vars/MYJenkinsfile.groovy
+++ vars/MYJenkinsfile.groovy
@@ -9,11 +9,12 @@ try {
  stage('checkout') {
     cleanWs()
     checkout scm
-    env.commit_hash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+    def commit_hash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
     println("Tag not specified, using the latest commit hash: " + commit_hash)
-    echo "${env.commit_hash}"
-    sh 'echo "$env.commit_hash"' 
-
+    echo "${commit_hash}"
+    sh 'echo "commit_hash"' 
+    sh 'echo $commit_hash'
+    
   }
    stage("GIT INFO"){
 
