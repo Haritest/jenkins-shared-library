a275fb8289d1c6e73a1a35c7bc35e96d5dda7092
haridas.kakunje <haridas.kakunje@tarento.com>
2019-05-16 11:07:42 +0530

    Jenkinsfile added

--- vars/MYJenkinsfile.groovy
+++ vars/MYJenkinsfile.groovy
@@ -12,14 +12,12 @@ try {
     env.commit_hash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
     println("Tag not specified, using the latest commit hash: " + commit_hash)
     echo "${env.commit_hash}"
-    sh 'echo ${env.commit_hash}' 
+    sh 'echo "$env.commit_hash"' 
 
   }
    stage("GIT INFO"){
 
-shortCommit = sh(returnStdout: true, script: "git log -n 1 --pretty=format:'%h'").trim()
 
-sh 'echo "$shortCommit"'
 }
 }
 catch (err) {
