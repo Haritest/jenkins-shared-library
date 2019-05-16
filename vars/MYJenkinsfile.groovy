2fba18e255b97cffcae11ce0b6e3d0b57cabaf16
haridas.kakunje <haridas.kakunje@tarento.com>
2019-05-16 09:31:11 +0530

    Jenkinsfile updated

--- vars/MYJenkinsfile.groovy
+++ vars/MYJenkinsfile.groovy
@@ -9,6 +9,8 @@ try {
  stage('checkout') {
     cleanWs()
     checkout scm
+    commit_hash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
+    println("Tag not specified, using the latest commit hash: " + commit_hash)
   }
    stage("GIT INFO"){
 
