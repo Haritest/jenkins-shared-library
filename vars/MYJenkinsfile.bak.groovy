3495eee7ad756abbb0c74a9a2597f66f9932f6c4
haridas.kakunje <haridas.kakunje@tarento.com>
2019-11-10 23:07:00 +0530

    Jenkinsfile-sonar deleted

--- vars/MYJenkinsfile.bak.groovy
+++ vars/MYJenkinsfile.bak.groovy
@@ -1,30 +1,15 @@
 def call(body) {
 body()
-node ('master') {
 
-try {
+node('master')
+{
+stage('Deploy')
 
- stage('checkout') {
-    cleanWs()
-    checkout scm
-    def commit_hash = sh(script: 'git rev-parse --short HEAD', returnStdout: true).trim()
-    println("Tag not specified, using the latest commit hash: " + commit_hash)
-    env.commit_hash = commit_hash
- //  sh 'echo "commit_hash"'    
- //   echo "${commit_hash}"
- //   sh 'echo "commit_hash"' 
-    sh 'echo $commit_hash'
-    
-  }
-   stage("GIT INFO"){
-   sh 'echo $commit_hash'
 
+     sh  '''
+            pwd
+         '''
 }
 }
-catch (err) {
-        currentBuild.result = "FAILURE"
-        throw err
-        }
 }
 
-}
